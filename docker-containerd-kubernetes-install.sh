94164cf7164587b6f8bdebd0af8d141f9d9ee825
harunakts <65662474+harunakts@users.noreply.github.com>
2023-02-26 19:39:52 +0300

    Update docker-containerd-kubernetes-install.sh

--- docker-containerd-kubernetes-install.sh
+++ docker-containerd-kubernetes-install.sh
@@ -39,7 +39,7 @@ sudo sysctl --system
 sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg \
   https://packages.cloud.google.com/apt/doc/apt-key.gpg
 
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" \
+echo "https://apt.kubernetes.io/ kubernetes-xenial main" \
   | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 sudo apt-get update
