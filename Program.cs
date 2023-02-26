7131c1751b778d75adb451755e2e341f1e43e114
harunakts <65662474+harunakts@users.noreply.github.com>
2023-02-26 19:17:53 +0300

    testttt
    testttt

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World Harun Aktaş Test");
+app.MapGet("/", () => "Bu bir test");
 
 app.Run();
