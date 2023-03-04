8c8f8798e0a5de47f6832e53d4a54d2a13822d0c
harunakts <65662474+harunakts@users.noreply.github.com>
2023-03-05 00:58:59 +0300

    bash çalıştırırken /'de hata vermesini engelleme
    bash çalıştırırken /'de hata vermesini engelleme

--- docker-containerd-kubernetes-install.sh
+++ docker-containerd-kubernetes-install.sh
@@ -3,24 +3,19 @@
 sudo apt update
 sudo apt install -y apt-transport-https ca-certificates curl
 #Containerd yükle
-curl -fsSLo containerd-config.toml \
-  https://gist.githubusercontent.com/oradwell/31ef858de3ca43addef68ff971f459c2/raw/5099df007eb717a11825c3890a0517892fa12dbf/containerd-config.toml
+curl -fsSLo containerd-config.toml https://gist.githubusercontent.com/oradwell/31ef858de3ca43addef68ff971f459c2/raw/5099df007eb717a11825c3890a0517892fa12dbf/containerd-config.toml
 sudo mkdir /etc/containerd
 sudo mv containerd-config.toml /etc/containerd/config.toml
-curl -fsSLo containerd-1.6.14-linux-amd64.tar.gz \
-  https://github.com/containerd/containerd/releases/download/v1.6.14/containerd-1.6.14-linux-amd64.tar.gz
+curl -fsSLo containerd-1.6.14-linux-amd64.tar.gz https://github.com/containerd/containerd/releases/download/v1.6.14/containerd-1.6.14-linux-amd64.tar.gz
 sudo tar Cxzvf /usr/local containerd-1.6.14-linux-amd64.tar.gz
-sudo curl -fsSLo /etc/systemd/system/containerd.service \
-https://raw.githubusercontent.com/containerd/containerd/main/containerd.service
+sudo curl -fsSLo /etc/systemd/system/containerd.service https://raw.githubusercontent.com/containerd/containerd/main/containerd.service
 sudo systemctl daemon-reload
 sudo systemctl enable --now containerd
 #Runc Yükle
-curl -fsSLo runc.amd64 \
-  https://github.com/opencontainers/runc/releases/download/v1.1.3/runc.amd64
+curl -fsSLo runc.amd64 https://github.com/opencontainers/runc/releases/download/v1.1.3/runc.amd64
 sudo install -m 755 runc.amd64 /usr/local/sbin/runc
 # CNI network plugins Yükleme
-curl -fsSLo cni-plugins-linux-amd64-v1.1.1.tgz \
-  https://github.com/containernetworking/plugins/releases/download/v1.1.1/cni-plugins-linux-amd64-v1.1.1.tgz
+curl -fsSLo cni-plugins-linux-amd64-v1.1.1.tgz https://github.com/containernetworking/plugins/releases/download/v1.1.1/cni-plugins-linux-amd64-v1.1.1.tgz
 sudo mkdir -p /opt/cni/bin
 sudo tar Cxzvf /opt/cni/bin cni-plugins-linux-amd64-v1.1.1.tgz
 #IPv4'ü iletin ve iptables'ın Bridge ağ trafiğini görmesine izin vermek için
@@ -36,11 +31,9 @@ net.ipv4.ip_forward                 = 1
 EOF
 sudo sysctl --system
 #kubeadm, kubelet & kubectl yükleme.
-sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg \
-  https://packages.cloud.google.com/apt/doc/apt-key.gpg
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 
-echo "https://apt.kubernetes.io/ kubernetes-xenial main" \
-  | sudo tee /etc/apt/sources.list.d/kubernetes.list
+echo "https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 sudo apt-get update
 
